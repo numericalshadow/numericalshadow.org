3bb6ab18d4c4b42a311349952021adb97c16d1ee
Łukasz Pawela <lukasz.pawela@gmail.com>
2022-02-06 23:24:31 +0100

    update built site

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker-compose run jekyll /bin/bash -c 'JEKYLL_ENV=production jekyll build'
+git checkout gh-pages
+cp -r _site/* . && rm -rf _site/ && touch .nojekyll
+git add *
+git commit -m 'update built site'
+git push
+git checkout master
